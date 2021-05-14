375dfd2843c0f4891ae4b1653a5b109de4a19814
nzx <1343390288@163.com>
2021-05-14 17:35:35 +0800

    add code

--- src/main/java/com/assemble/controller/UserController.java
+++ src/main/java/com/assemble/controller/UserController.java
@@ -16,7 +16,6 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
     @GetMapping("/list")
     public List<UserDTO> list(){
         return  userService.list();
